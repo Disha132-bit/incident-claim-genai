5eec95ec7f0757719c9d8899d758d19291215a4f
Disha132-bit <dishawaghmare209@gmail.com>
2025-06-28 09:45:37 +0530

    Update 6_About.py

--- app/pages/6_About.py
+++ app/pages/6_About.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+st.title("ℹ️ About This Project")
+
+st.markdown("""
+This is a GenAI-powered Insurance Claim Analyzer built using:
+
+🧠 LLaMA model for summarization
+
+📄 NLP-based extraction
+
+⚠️ Rule-based fraud detection
+
+🎨 Streamlit for interactive UI
+
+Created to assist insurers in automating document analysis, improving decision speed, and detecting anomalies early.
+""")
